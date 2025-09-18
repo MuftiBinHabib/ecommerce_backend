c77ebe567923f98dcd2513a5acc582e78a52736c
MuftiBinHabib <muftibinhabib500@gmail.com>
2025-09-19 00:55:51 +0600

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require("express")
+const dbConnection = require("./src/config/dbconfig")
+const router = require("./src/route")
+
+
+const app = express()
+const port = process.env.PORT || 4000
+//database connection
+dbConnection()
+
+// http://localhost:3000
+
+//router middleware
+app.use(router)
+
+// page not found middleware
+
+app.use((req,res) =>{
+  return res.status(404).json({message: "route not found"})
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running port number ${port}`)
+})
---
+++ src/route/api/auth/index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+// http://localhost:3000/api/v1/auth/signup
+router.post("/signup" , (req,res)=>{
+    return res.send("signup")
+})
+
+module.exports = router
